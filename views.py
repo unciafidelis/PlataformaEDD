72c6dab6ee7e80fff5366271f907ff3b28333bca
Alejandro Morgan <MY_NAME@example.com>
2025-01-22 11:01:15 -0600

    Update index.html

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template, request
+from flask import Blueprint,render_template,request,jsonify,redirect,url_for 
 
 views = Blueprint(__name__, "views")
 
@@ -7,8 +7,20 @@ def home():
     return render_template("index.html", clase="Estructura de datos")
 
 @views.route("/tema")
-
 def tema():
     args = request.args
-    name = args.get('Sección?')
+    name = args.get('seccion')
     return render_template("index.html", seccion=name)
+
+@views.route("/json")
+def get_json():
+    return jsonify({'Arreglos Unidimensionales':'Son divertidos'})
+
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+@views.route("/go_home")
+def go_home():
+    return redirect(url_for("views.home"))
